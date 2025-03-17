656be879a00d150d10e48fa000f13098b68fe6f3
Sean-Lim-Yong-Xuan <168115550+Sean-Lim-Yong-Xuan@users.noreply.github.com>
2025-03-17 09:00:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def display_data_from_mongodb():
             #df = pd.DataFrame(data)
             #df = df.drop(columns=['_id'], errors='ignore') #remove mongodb's _id
             #st.dataframe(df)
-            st.info ("Successfully connected to MongoDB!!")
+            st.write("Successfully connected to MongoDB!!")
         else:
             st.info("No data found in MongoDB.")
     except Exception as e:
@@ -81,12 +81,10 @@ if allmtgcards:
     chart_option = st.selectbox("Select a chart type:", ["Color Identity Distribution", "Type Distribution", "Power Distribution"])
     
     if chart_option == "Color Distribution":
-        st.info ("Data Obtained")
+        st.write ("Data Obtained")
         fig = px.bar(filtered_df["color_identity"].value_counts().reset_index(), x="count", y="Color", title="Color Identity Distribution")
         st.plotly_chart(fig)
     #elif chart_option == "Type Distribution":
         #fig = px.bar(filtered_df["type"].value_counts().reset_index(), x="count", y="Type", title="Type Distribution")
     #elif chart_option == "Power Distribution":
         #fig = px.bar(filtered_df["power"].value_counts().reset_index(), x="count", y="Power", title="Power Distribution")
-else:
-    st.warning ("NO DATA FOUND!")
