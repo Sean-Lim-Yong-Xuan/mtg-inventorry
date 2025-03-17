caec4c02f63378ffbaf0c2263d8c95a503385d5d
Sean-Lim-Yong-Xuan <168115550+Sean-Lim-Yong-Xuan@users.noreply.github.com>
2025-03-17 09:36:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,8 +83,10 @@ if allmtgcards:
     if chart_option == "Color Distribution":
         st.write ("Data Obtained")
         fig = px.bar(filtered_df["color_identity"].value_counts().reset_index(), x="count", y="Color", title="Color Identity Distribution")
-    st.plotly_chart(fig)
+        st.plotly_chart(fig)
     #elif chart_option == "Type Distribution":
         #fig = px.bar(filtered_df["type"].value_counts().reset_index(), x="count", y="Type", title="Type Distribution")
     #elif chart_option == "Power Distribution":
         #fig = px.bar(filtered_df["power"].value_counts().reset_index(), x="count", y="Power", title="Power Distribution")
+    else:
+        st.write ("No chart.")
